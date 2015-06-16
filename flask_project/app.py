59f71f3aa5f7dbf6664ea214256862cfbfe97c25
simanto604newscred <simanto605@gmail.com>
2015-06-16 17:45:37 +0600

    first deploy

--- flask_project/app.py
+++ flask_project/app.py
@@ -11,7 +11,7 @@ def index():
 @app.route('/data')
 def names():
     data = {
-        "first_names": ["John", "Jacob", "Julie", "Jenny", "Another"]
+        "first_names": ["John", "Jacob", "Julie", "Jenny", "Another boy"]
     }
     return jsonify(data)
 
