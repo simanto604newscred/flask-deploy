5ac9ec26bf6147df9c5d5c450cb3f79666464d2d
simanto604newscred <simanto605@gmail.com>
2015-07-02 00:53:41 +0600

    App first Release

--- flask_project/app.py
+++ flask_project/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, send_file, render_template
-from scraptest import Legacy
+from main_pilot import MainPilot
 from flask.ext.mail import Mail, Message
 app = Flask(__name__)
-import os
+import os, threading, time
 
 # Flask-Mail configuration
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
@@ -15,6 +15,45 @@ app.config['MAIL_DEFAULT_SENDER'] = 'simanto605@gmail.com'
 
 # Initialize extensions
 mail = Mail(app)
+
+flag = True
+
+def set_globvar_to(value):
+    global flag    # Needed to modify global copy of globvar
+    flag = value
+
+def get_globvar():
+    global flag
+    return flag
+
+
+def send_email():
+    msg = Message("Hello",
+                  sender="simanto605@gmail.com",
+                  recipients=["simanto604@gmail.com" ])
+    # import ipdb;ipdb.set_trace()
+    msg.body = "testing"
+    msg.html = "<b>testing</b>"
+    with app.open_resource("test.csv") as fp:
+        msg.attach("test.csv", "text/csv", fp.read())
+    with app.open_resource("sample.csv") as fd:
+        msg.attach("sample.csv", "text/csv", fd.read())
+    with app.open_resource("result.csv") as fr:
+        msg.attach("result.csv", "text/csv", fr.read())
+    with app.app_context():
+        mail.send(msg)
+
+def foo():
+    print "f started"
+
+    if get_globvar():
+        set_globvar_to(False)
+        main_pilot = MainPilot()
+        # main_pilot.run()
+        send_email()
+        set_globvar_to(True)
+    print "f finished"
+
 
 @app.route('/')
 def index():
@@ -39,28 +78,24 @@ def get_file():
 def get_another_file():
     return send_file('doc.csv')
 
-@app.route('/in-progress')
+@app.route('/mail')
 def progress():
-    msg = Message("Hello",
-                  sender="simanto605@gmail.com",
-                  recipients=["simanto604@gmail.com" ])
-    msg.body = "testing"
-    msg.html = "<b>testing</b>"
-    with app.open_resource("test.csv") as fp:
-        msg.attach("test.csv", "text/csv", fp.read())
-    with app.open_resource("sample.csv") as fd:
-        msg.attach("sample.csv", "text/csv", fd.read())
-    with app.open_resource("result.csv") as fr:
-        msg.attach("result.csv", "text/csv", fr.read())
+    thr = threading.Thread(target=foo, args=(), kwargs={})
+    # import ipdb;ipdb.set_trace()
+    if not thr.is_alive():
+        thr.start() # will run "foo"
 
-    mail.send(msg)
+    # thr.is_alive() # will return whether foo is running currently
 
-    return 'This part is in progress....'
+    # thr.join()
+
+    return "You'll get a mail...."
 
 @app.route('/report')
 def create_report():
+
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- flask_project/main_pilot.py
+++ flask_project/main_pilot.py
@@ -1,57 +1,60 @@
 from scraptest import Legacy
-import csv
+import csv, os
 import operator
 from beenVerified import BeenVerified
 from crossmatch import CrossMatch
 
 class MainPilot:
-    legacy_search = Legacy()
-    legacy_search.search()
-    f = open('result.csv','w')
-    a_writter = csv.writer(f)
-    been_verified = BeenVerified()
-    with open('test.csv', 'rb') as csvfile:
-        testreader = csv.reader(csvfile)
-        for row in testreader:
-            print row
-            been_verified.search(row[0])
-            result_dict = {}
+    def run(self):
+        legacy_search = Legacy()
+        legacy_search.search()
+        f = open('result.csv','w')
+        a_writter = csv.writer(f)
+        been_verified = BeenVerified()
+        try:
+            os.remove('doc.csv')
+        except OSError:
+            pass
+        with open('test.csv', 'rb') as csvfile:
+            testreader = csv.reader(csvfile)
+            for row in testreader:
+                print row
+                been_verified.search(row[0])
+                result_dict = {}
 
-            with open('sample.csv', 'rb') as another_csvfile:
-                sample_reader= csv.reader(another_csvfile)
-                for index,row_in_sample in enumerate(sample_reader):
-                    print row_in_sample
-                    crossmatch = CrossMatch()
-                    obituary = row[2]
-                    try:
-                        if row_in_sample[4]:
-                            score = crossmatch.extract_entities(obituary, row_in_sample[4].split('#'))
-                            print score
-                            if score:
-                                result_dict[index] = [score,row_in_sample]
+                with open('sample.csv', 'rb') as another_csvfile:
+                    sample_reader= csv.reader(another_csvfile)
+                    for index,row_in_sample in enumerate(sample_reader):
+                        print row_in_sample
+                        crossmatch = CrossMatch()
+                        obituary = row[2]
+                        try:
+                            if row_in_sample[4]:
+                                score = crossmatch.extract_entities(obituary, row_in_sample[4].split('#'))
+                                print score
+                                if score:
+                                    result_dict[index] = [score,row_in_sample]
 
-                    except IndexError:
-                        continue
+                        except IndexError:
+                            continue
 
-                if bool(result_dict):
-                    dict_key = max(result_dict.iteritems(), key=operator.itemgetter(1))[0]
-                    a_writter.writerows([result_dict[dict_key][1]])
-                    print "One result written"
+                    if bool(result_dict):
+                        dict_key = max(result_dict.iteritems(), key=operator.itemgetter(1))[0]
+                        a_writter.writerows([result_dict[dict_key][1]])
+                        print "One result written"
 
-            print '***********One iteration finished break************'
-
-
-
-
-
-
-
+                print '***********One iteration finished break************'
 
 
 
 
 
-if __name__ == '__main__':
 
-    pass
+
+
+
+
+
+
+
 
